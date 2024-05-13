c9cbde36268a04fdb684ffa42436a48e7580485f
elturanshucai <elturanfcb@gmail.com>
2024-05-12 22:46:25 -0700

    course deleting handled

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -46,8 +46,9 @@ export const deleteCourse = async (req, res) => {
         if (role === 'teacher' && userId !== course.ownerId) return res.status(401).json('you can only delete your own course')
         const courseBuyedList = await BuyCourse.find({ courseId: req.params.id })
         courseBuyedList.map(async item => await BuyCourse.findByIdAndDelete(item._id))
-        await Course.findByIdAndUpdate(req.params.id, { enable: !course.enable }, { new: true })
-        res.status(200).json("Course deleted")
+        if (!course.accepted) return res.status(400).json('Course waiting for accept!')
+        const changedCourse = await Course.findByIdAndUpdate(req.params.id, { enable: !course.enable, accepted: course.enable ? false : true }, { new: true })
+        res.status(200).json(changedCourse)
     } catch (error) {
         res.status(500).json('Internal Server Error')
     }
