a32104848d6962bfbaea31980dafd6c9d53dc50f
elturanshucai <elturanfcb@gmail.com>
2025-03-01 16:04:12 +0400

    middlware

--- utils/AdminMiddleware.js
+++ utils/AdminMiddleware.js
@@ -6,13 +6,13 @@ export const verifyAdmin = async (req, res, next) => {
         const token = req.headers.authorization?.split(" ")[1]
         if (!token) return res.status(401).json('Unauthorized')
         jwt.verify(token, process.env.JWT_SECRET, async (err, data) => {
-            if (err) return res.status(403).json('Token is not valid')
+            if (err) return res.status(401).json('Token is not valid')
             const user = await User.findById(data.id)
             if (user?.role === 'admin') {
                 req.userId = data.id
                 return next()
             }
-            return res.status(403).json('You are not admin')
+            return res.status(401).json('You are not admin')
         })
     } catch (error) {
         console.log(error)
--- utils/LoginMiddleware.js
+++ utils/LoginMiddleware.js
@@ -6,9 +6,9 @@ export const verifyLogin = async (req, res, next) => {
         const token = req.headers.authorization?.split(" ")[1]
         if (!token) return res.status(401).json('Unauthorized')
         jwt.verify(token, process.env.JWT_SECRET, async (err, data) => {
-            if (err) return res.status(403).json('Token is not valid')
+            if (err) return res.status(401).json('Token is not valid')
             const user = await User.findById(data.id)
-            if (!user) return res.status(404).json('User not found')
+            if (!user) return res.status(401).json('User not found')
             req.userId = data.id;
             req.userRole = user.role
             return next()
