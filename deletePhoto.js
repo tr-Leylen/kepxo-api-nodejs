33b3383ff6bdcfd333b69e2c3a9f42957c861907
elturanshucai <elturanfcb@gmail.com>
2025-03-16 23:08:59 +0400

    fix

--- deletePhoto.js
+++ deletePhoto.js
@@ -7,15 +7,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const deletePhoto = async (url) => {
-    if (!url) return
-    const photo = await Photo.findOne({ url })
-    const fileName = photo?.fileName
-    const filepath = path.join(__dirname, process.env.UPLOADS_DIR, fileName);
-    fs.unlink(filepath, async (err) => {
-        if (err) {
-            console.log(err)
-            return;
-        }
-        await Photo.deleteOne({ url })
-    });
+    try {
+        if (!url) return
+        const photo = await Photo.findOne({ url })
+        const fileName = photo?.fileName
+        const filepath = path.join(__dirname, process.env.UPLOADS_DIR, fileName);
+        fs.unlink(filepath, async (err) => {
+            if (err) {
+                console.log(err)
+                return;
+            }
+            await Photo.deleteOne({ url })
+        });
+    } catch (error) {
+        console.log(error)
+    }
 }
