797b2c7b8bfbcca7c32c8f3cf8e127be36651be3
elturanshucai <elturanfcb@gmail.com>
2025-03-23 06:46:13 +0400

    test

--- deletePhoto.js
+++ deletePhoto.js
@@ -11,7 +11,7 @@ export const deletePhoto = async (url) => {
         if (!url) return
         const photo = await Photo.findOne({ url })
         const fileName = photo?.fileName
-        const filepath = path.join(uploadsFolder, fileName);
+        const filepath = path.join(String(uploadsFolder), fileName);
         fs.unlink(filepath, async (err) => {
             if (err) {
                 console.log(err)
