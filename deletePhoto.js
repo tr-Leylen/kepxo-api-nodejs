34083ad7381f0fa790e2a58ad27558346b042796
elturanshucai <elturanfcb@gmail.com>
2025-03-23 06:44:18 +0400

    fix

--- deletePhoto.js
+++ deletePhoto.js
@@ -6,11 +6,12 @@ import fs from 'fs'
 const uploadsFolder = process.env.SERVER_UPLOADS
 
 export const deletePhoto = async (url) => {
+    console.log(uploadsFolder)
     try {
         if (!url) return
         const photo = await Photo.findOne({ url })
         const fileName = photo?.fileName
-        const filepath = path.join(`${uploadsFolder}`, fileName);
+        const filepath = path.join(uploadsFolder, fileName);
         fs.unlink(filepath, async (err) => {
             if (err) {
                 console.log(err)
